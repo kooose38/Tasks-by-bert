70f79294c804036051b85b254db8bea4569a026a
kooose38 <kooose0308@gmail.com>
2021-07-24 18:19:02 +0900

    create tutorial.md from grammer

--- grammer/code/debug.py
+++ grammer/code/debug.py
@@ -2,9 +2,9 @@ import sys
 import predict 
 args = sys.argv 
 text = args[1]
-best_model_path = "./model/###"
+best_model_path = "./model/best_model.ckpt"
 
-predict_text = predict.predict(text, best_model_path)
+predict_text = predict.predict(text, best_model_path, cuda=False)
 print("--")
 print(f"input: {text}")
 print(f"output: {predict_text}")
